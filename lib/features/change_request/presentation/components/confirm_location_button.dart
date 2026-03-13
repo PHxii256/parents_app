f05dc1e3312a53b7d3ff40ba3f9f64c8ecd91732
PHxii256 <69358352+PHxii256@users.noreply.github.com>
2026-03-13 20:49:41 +0200

    Add an optional address line form field to add location pop up

--- lib/features/change_request/presentation/components/confirm_location_button.dart
+++ lib/features/change_request/presentation/components/confirm_location_button.dart
@@ -8,10 +8,25 @@ class ConfirmLocationButton extends StatefulWidget {
 }
 
 class _ConfirmLocationButtonState extends State<ConfirmLocationButton> {
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _locationNameController = TextEditingController();
+  final TextEditingController _locationAdressLineController = TextEditingController();
+
+  @override
+  void dispose() {
+    _locationNameController.dispose();
+    _locationAdressLineController.dispose();
+    super.dispose();
+  }
+
+  void submitLocation(BuildContext c) {
+    // to:do call a method on a bloc to save it to the backend
+    Navigator.of(c).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final isValid = _formKey.currentState?.validate() ?? false;
     return Positioned(
       left: 80,
       right: 80,
@@ -25,39 +40,66 @@ class _ConfirmLocationButtonState extends State<ConfirmLocationButton> {
               context: context,
               builder: (c) => AlertDialog(
                 titlePadding: EdgeInsets.fromLTRB(16, 28, 16, 0),
-                contentPadding: EdgeInsets.fromLTRB(16, 16, 16, 28),
+                contentPadding: EdgeInsets.fromLTRB(16, 16, 16, 8),
+                actionsPadding: EdgeInsets.fromLTRB(16, 0, 16, 16),
+
                 title: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: const Text(
-                    'Enter a Name For This Location',
-                    style: TextStyle(fontSize: 18),
-                  ),
+                  child: const Text('Add Location Details', style: TextStyle(fontSize: 18)),
                 ),
+                actions: [
+                  TextButton(onPressed: isValid ? () => submitLocation : null, child: Text("Done")),
+                ],
                 content: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: TextField(
-                    controller: _locationNameController,
-                    decoration: InputDecoration(
-                      hintText: "eg. Grandma's Home",
-                      suffixIcon: IconButton.filled(
-                        onPressed: () {
-                          debugPrint("New Location: ${_locationNameController.text}");
-                          Navigator.of(c).pop();
-                        },
-                        style: ButtonStyle(
-                          backgroundColor: WidgetStatePropertyAll(Colors.transparent),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                          controller: _locationNameController,
+                          validator: (value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return 'Location name is required';
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            floatingLabelBehavior: FloatingLabelBehavior.always,
+
+                            labelText: "Location Name",
+                            hintText: "eg. Grandma's Home",
+                            filled: true,
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2, color: AppColors.brownBg),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
                         ),
-                        icon: Icon(Icons.check, color: AppColors.brownBg.withAlpha(180)),
-                      ),
-                      filled: true,
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(width: 2, color: AppColors.brownBg),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(width: 2),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
+                        SizedBox(height: 16),
+                        TextFormField(
+                          controller: _locationAdressLineController,
+                          decoration: InputDecoration(
+                            floatingLabelBehavior: FloatingLabelBehavior.always,
+                            labelText: "Address (Optional)",
+                            hintText: "eg. Sakanat El-Maadi St.9 Building 31",
+                            filled: true,
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2, color: AppColors.brownBg),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 2),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
