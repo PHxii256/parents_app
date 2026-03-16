3dc05d930853a1b50d5d250842ead4e2864772c0
Fajr Abdulrhman <fajrabdulrhman@Fajrs-MacBook-Air.local>
2026-03-16 02:05:12 +0200

    edit profile

--- lib/features/profile/presentation/profile_state.dart
+++
@@ -1,9 +0,0 @@
-import 'package:parent_app/features/profile/domain/child.dart';
-
-sealed class ProfileState {}
-
-class Loading extends ProfileState {}
-
-class Success extends ProfileState {}
-
-class Error extends ProfileState {}
